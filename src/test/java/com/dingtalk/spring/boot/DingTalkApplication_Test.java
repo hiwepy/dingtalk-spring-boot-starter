d6c4f96646b6c5e3079d29b02f2c0c7f900b6638
luffy <luffy@vanillafinance.xyz>
2024-10-07 10:33:45 +0800

    3.3.x.20241003.RELEASE

--- src/test/java/com/dingtalk/spring/boot/DingTalkApplication_Test.java
+++ src/test/java/com/dingtalk/spring/boot/DingTalkApplication_Test.java
@@ -15,8 +15,8 @@
  */
 package com.dingtalk.spring.boot;
 
-import javax.annotation.PostConstruct;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
